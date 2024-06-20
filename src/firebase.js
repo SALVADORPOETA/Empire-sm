929bb8ba7219cf4481e4a8f5f68c8ab2f71921a9
unknown <dsml@live.com>
2024-06-20 10:15:35 -0600

    env

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,24 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+const apiKey = process.env.REACT_APP_API_KEY
+const authDomain = process.env.REACT_APP_AUTH_DOMAIN
+const projectId = process.env.REACT_APP_PROJECT_ID
+const storageBucket = process.env.REACT_APP_STORAGE_BUCKET
+const messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID
+const appId = process.env.REACT_APP_APP_ID
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAp7va20c-H8C5hDtVVBSPqVz_Y_KbwEEM",
-  authDomain: "empire-bc434.firebaseapp.com",
-  projectId: "empire-bc434",
-  storageBucket: "empire-bc434.appspot.com",
-  messagingSenderId: "896013042961",
-  appId: "1:896013042961:web:97750dbf509c5ca01bf8b0"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId
 };
+
+console.log("Firebase Config: ", firebaseConfig)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
