d5d77ad0b3f5e9b6cfb5ac665abc135058d2ce32
unknown <dsml@live.com>
2024-07-14 03:32:54 -0600

    images height

--- src/routes/Culture/RomeArts.jsx
+++ src/routes/Culture/RomeArts.jsx
@@ -3,23 +3,36 @@ import arts from '../../assets/arts.jpg'
 
 const RomeArts = () => {
   return (
-    <div className='bg-[var(--primary-red)] text-[var(--primary-gold)] py-16 px-4'>
-      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-        <a href="https://en.wikipedia.org/wiki/Ancient_Rome#Art,_music_and_literature" target='blank'>
-          <div className='flex flex-col justify-center m-8 hover:scale-105 transition-transform duration-300 bg-black/50 p-6 rounded-3xl'>
-            <p className='font-bold'>ROMAN ARTS</p>
-            <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Surviving frescoes used to adorn the walls and ceilings of country villas.</h1>
+    <div className="bg-[var(--primary-red)] text-[var(--primary-gold)] py-16 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+        <a
+          href="https://en.wikipedia.org/wiki/Ancient_Rome#Art,_music_and_literature"
+          target="blank"
+        >
+          <div className="flex flex-col justify-center m-8 hover:scale-105 transition-transform duration-300 bg-black/50 p-6 rounded-3xl">
+            <p className="font-bold">ROMAN ARTS</p>
+            <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+              Surviving frescoes used to adorn the walls and ceilings of country
+              villas.
+            </h1>
             <p>
-              Roman literature includes mentions of paintings on wood, ivory, and other materials.
-              Portrait sculpture used youthful and classical proportions, evolving later into a mixture of realism and idealism.
-              Roman music was largely based on Greek music.
-              Latin literature was, from its start, influenced heavily by Greek authors.
-              Some of the earliest extant works are of historical epics telling the early military history of Rome. 
-              As the Republic expanded, authors began to produce poetry, comedy, history, and tragedy.
+              Roman literature includes mentions of paintings on wood, ivory,
+              and other materials. Portrait sculpture used youthful and
+              classical proportions, evolving later into a mixture of realism
+              and idealism. Roman music was largely based on Greek music. Latin
+              literature was, from its start, influenced heavily by Greek
+              authors. Some of the earliest extant works are of historical epics
+              telling the early military history of Rome. As the Republic
+              expanded, authors began to produce poetry, comedy, history, and
+              tragedy.
             </p>
           </div>
         </a>
-        <img className='h-[500px] p-4 mx-auto my-4 rounded-[20%]' src={arts} alt='/' />
+        <img
+          className="h-[300px] md:h-[400px] lg:h-[500px] p-4 mx-auto my-4 rounded-[20%]"
+          src={arts}
+          alt="/"
+        />
       </div>
     </div>
   )
--- src/routes/Culture/RomeReligion.jsx
+++ src/routes/Culture/RomeReligion.jsx
@@ -3,24 +3,35 @@ import religion from '../../assets/religion.jpg'
 
 const RomeReligion = () => {
   return (
-    <div className='bg-[var(--primary-red)] text-[var(--primary-gold)] py-16 px-4'>
-      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-        <a href='https://en.wikipedia.org/wiki/Ancient_Rome#Religion' target='blank'>
-          <div className='flex flex-col justify-center m-8 hover:scale-105 transition-transform duration-300 bg-black/50 p-6 rounded-3xl'>
-            <p className='font-bold'>ROMAN RELIGION</p>
-            <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Archaic Roman religion, was made up of complex interrelations between gods and humans.</h1>
+    <div className="bg-[var(--primary-red)] text-[var(--primary-gold)] py-16 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+        <a
+          href="https://en.wikipedia.org/wiki/Ancient_Rome#Religion"
+          target="blank"
+        >
+          <div className="flex flex-col justify-center m-8 hover:scale-105 transition-transform duration-300 bg-black/50 p-6 rounded-3xl">
+            <p className="font-bold">ROMAN RELIGION</p>
+            <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+              Archaic Roman religion, was made up of complex interrelations
+              between gods and humans.
+            </h1>
             <p>
-              Unlike in Greek mythology, the gods were not personified, 
-              but were vaguely defined sacred spirits called numina. 
-              Romans also believed that every person, place or thing had its own genius, or divine soul. 
-              During the Roman Republic, Roman religion was organised under a strict system of priestly offices, 
-              which were held by men of senatorial rank. 
-              The College of Pontifices was uppermost body in this hierarchy, 
-              and its chief priest, the Pontifex Maximus, was the head of the state religion.          
+              Unlike in Greek mythology, the gods were not personified, but were
+              vaguely defined sacred spirits called numina. Romans also believed
+              that every person, place or thing had its own genius, or divine
+              soul. During the Roman Republic, Roman religion was organised
+              under a strict system of priestly offices, which were held by men
+              of senatorial rank. The College of Pontifices was uppermost body
+              in this hierarchy, and its chief priest, the Pontifex Maximus, was
+              the head of the state religion.
             </p>
           </div>
         </a>
-        <img className='h-[500px] p-4 mx-auto my-4 rounded-[20%]' src={religion} alt='/' />
+        <img
+          className="h-[300px] md:h-[400px] lg:h-[500px] p-4 mx-auto my-4 rounded-[20%]"
+          src={religion}
+          alt="/"
+        />
       </div>
     </div>
   )
